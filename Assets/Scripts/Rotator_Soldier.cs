7c7229daf218844058c45aa7f5de981c8f0c8176
Revertigo <dekel241@gmail.com>
2020-10-30 13:09:20 +0200

    Add soldier movement

---
+++ Assets/Scripts/Rotator_Soldier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class Rotator_Soldier : MonoBehaviour
+{
+    float speed = -1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+}
